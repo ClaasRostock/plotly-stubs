6c5f21995356260c49172edaa199b532e270e376
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2025-07-29 16:54:55 +0200

    added all missing type arguments, deleted TrendLineOptions to avoid any confusion

--- src/plotly-stubs/express/_stubs_helpers.pyi
+++ src/plotly-stubs/express/_stubs_helpers.pyi
@@ -13,15 +13,11 @@ class DataFrameCompatible(Protocol):
     # More details at https://data-apis.org/dataframe-protocol/latest/index.html
     def __dataframe__(self, nan_as_null: bool = ..., allow_copy: bool = ...) -> Any: ...
 
-DataArray: TypeAlias = Sequence[Any] | pd.Series | pl.Series | NDArray[Any]
-FrameOrDict: TypeAlias = DataFrameCompatible | dict[str, DataArray] | Sequence[dict[str, Any]]
-ColumnData: TypeAlias = str | int | DataArray
+ArrayLike: TypeAlias = Sequence[Any] | pd.Series | pl.Series | NDArray[Any]
+FrameOrDict: TypeAlias = DataFrameCompatible | dict[str, ArrayLike] | Sequence[dict[str, Any]]
+ColumnData: TypeAlias = str | int | ArrayLike
 MultiColumnData: TypeAlias = ColumnData | list[ColumnData]
-HoverData: TypeAlias = ColumnData | list[str] | dict[str, bool | str | DataArray]
-
-TrendLineOptions: TypeAlias = dict[
-    str, Any
-]  # Specified to better spot remaining "any" types, and facilitating eventual future improvements.
+HoverData: TypeAlias = ColumnData | list[str] | dict[str, bool | str | ArrayLike]
 MapIdentity: TypeAlias = (
     dict[str | int, str] | Literal["identity"]
 )  # Would rather use dict[str, str] | dict[int, str], but is currently technically correct for pandas.
@@ -41,6 +37,8 @@ LineShape: TypeAlias = Literal["linear", "spline", "hv", "vh", "hvh", "vhv"]
 Orientation: TypeAlias = Literal["v", "h"]
 DisplayMode: TypeAlias = Literal["group", "overlay"]
 LocationMode: TypeAlias = Literal["ISO-3", "USA-states", "country names"]
+BranchVals: TypeAlias = Literal["total", "remainder"]
+FitBounds: TypeAlias = Literal["locations", "geojson", "False"]
 Templates: TypeAlias = Literal[
     "ggplot2",
     "seaborn",
@@ -53,4 +51,62 @@ Templates: TypeAlias = Literal[
     "ygridoff",
     "gridon",
     "none",
+]
+Projection: TypeAlias = Literal[
+    "equirectangular",
+    "mercator",
+    "orthographic",
+    "natural earth",
+    "kavrayskiy7",
+    "miller",
+    "robinson",
+    "eckert4",
+    "azimuthal equal area",
+    "azimuthal equidistant",
+    "conic equal area",
+    "conic conformal",
+    "conic equidistant",
+    "gnomonic",
+    "stereographic",
+    "mollweide",
+    "hammer",
+    "transverse mercator",
+    "albers usa",
+    "winkel tripel",
+    "aitoff",
+    "sinusoidal",
+]
+GeoScope: TypeAlias = Literal["world", "usa", "europe", "asia", "africa", "north america"]
+MapStyle: TypeAlias = Literal[
+    "basic",
+    "carto-darkmatter",
+    "carto-darkmatter-nolabels",
+    "carto-positron",
+    "carto-positron-nolabels",
+    "carto-voyager",
+    "carto-voyager-nolabels",
+    "dark",
+    "light",
+    "open-street-map",
+    "outdoors",
+    "satellite",
+    "satellite-streets",
+    "streets",
+    "white-bg",
+]
+MapBoxStyle: TypeAlias = Literal[
+    "open-street-map",
+    "white-bg",
+    "carto-positron",
+    "carto-darkmatter",
+    "basic",
+    "streets",
+    "outdoors",
+    "light",
+    "dark",
+    "satellite",
+    "satellite-streets",
+    "stamen-terrain",
+    "stamen-toner",
+    "stamen-watercolor",
 ]
